bf89af0dd847fde97cd7af1b61e1fe47dcfa462e
Jules Boiteux <julesboiteux@gmail.com>
2018-11-27 17:16:01 +0100

    changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :flats do
-    resources :bookings
+    resources :bookings do
     resources :reviews, only: [:new, :create, :destroy, :index]
-  end
+    end
+  endroit
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
