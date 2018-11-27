8afa380938f664f8ec91c47e49776c71c5d76d04
Jules Boiteux <julesboiteux@gmail.com>
2018-11-27 15:33:39 +0100

    generated reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :flats do
     resources :bookings
-    resources :reviews, only: [:new, :create, :delete, :index]
+    resources :reviews, only: [:new, :create, :destroy, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
