42ba02a537565fd21654d3168279792e0c9f7a43
abdelkrim djoudi <abdelkrim.djoudi@gmail.com>
2018-11-27 17:43:20 +0100

    bookings destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,44 +1,49 @@
 class BookingsController < ApplicationController
+  before_action :set_flat
+
   def index
     @bookings = Booking.all
   end
 
-  def show
-
-  end
+  # def show
+  #   @booking = Booking.find(params[:id])
+  # end
 
   def new
-    @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
   end
 
   def create
-    @flat = Flat.find(params[:flat_id])
     @booking = Booking.new(booking_params)
     @booking.flat = @flat
 
     @booking.user = current_user
     if @booking.save!
-      redirect_to flat_path(@flat)
+      redirect_to flat_bookings_path(@flat)
     else
       render :new
     end
   end
-  def edit
+  # def edit
 
-  end
+  # end
 
-  def update
-
-  end
+  # # def update
+  # end
 
   def destroy
-
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to flat_bookings_path(@flat)
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
   end
 end
