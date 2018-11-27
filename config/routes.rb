4dda338e37e3b1432bb2ee65317894dc5e0d3fe0
Jules Boiteux <julesboiteux@gmail.com>
2018-11-27 17:20:50 +0100

    changed the error

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :bookings do
     resources :reviews, only: [:new, :create, :destroy, :index]
     end
-  endroit
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
